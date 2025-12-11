e1978afcc37cd01da18b6af5da7476bce1dbfa34
Jan Vrany <jan.vrany@labware.com>
2025-12-11 14:52:17 +0000

    Update dev tools installed by default
    This commit updates set of dev tools and libraries to match project I
    work on more often. This makes it easier for me to rebuild an image
    and test.

--- common/mk-fs-hooks/customize50-dev-tools.sh
+++ common/mk-fs-hooks/customize50-dev-tools.sh
@@ -19,18 +19,83 @@ chroot "${ROOT}" /usr/bin/apt-get --allow-unauthenticated -y install \
 	build-essential \
 	git cvs mercurial \
 	autoconf file cmake ninja-build pkg-config ccache \
-	flex bison texinfo gawk mawk \
+	flex bison gawk mawk \
 	gdb tmux
 
 #
 # Install -dev packages
 #
-chroot "${ROOT}" /usr/bin/apt-get --allow-unauthenticated -y install \
-	libglib2.0-dev \
-	python3-dev libexpat1-dev libncurses-dev libncurses5-dev libncursesw5-dev \
-	libdwarf-dev libelf-dev \
-	libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev \
-	libasound2-dev libcups2-dev libfontconfig1-dev
+#
+# Packages required to build (selected) projects
+#
+PACKAGES_GDB="  python3-dev,
+                libexpat1-dev,
+                libncurses-dev,
+                libncurses5-dev,
+                libncursesw5-dev,
+                libdebuginfod-dev,
+                libgmp-dev,
+                libmpfr-dev,
+                liblzma-dev,
+                libreadline-dev,
+                guile-3.0-dev,
+                texinfo
+            "
+PACKAGES_GDB_TEST="
+                dejagnu,
+                python3-unidiff,
+                python3-termcolor,
+                gfortran,
+                gnat,
+                rustc,
+                libc6-dbg
+            "
+
+#
+# See https://jan.vrany.io/stx/wiki/Documentation/BuildingStXWithRakefiles#Debianonx86_64andotherderivativessuchasUbuntuorMint
+#
+PACKAGES_STX="  pkg-config
+                libc6-dev
+                libx11-dev
+                libxext-dev
+                libxinerama-dev
+                unixodbc-dev
+                libgl1-mesa-dev
+                libfl-dev
+                libxft-dev
+                libbz2-dev
+                zlib1g
+                zlib1g-dev
+                libxfixes-dev
+            "
+
+PACKAGES_OMR="  zlib1g
+                zlib1g-dev
+                libglib2.0-dev
+                libdwarf-dev
+                libelf-dev
+                libx11-dev
+                libxext-dev
+                libxrender-dev
+                libxrandr-dev
+                libxtst-dev
+                libxt-dev
+                libasound2-dev
+                libcups2-dev
+                libfontconfig1-dev
+                file
+                "
+
+PACKAGES="      ${PACKAGES_GDB}
+                ${PACKAGES_GDB_TEST}
+                ${PACKAGES_STX}
+                ${PACKAGES_OMR}
+                "
+
+
+
+
+chroot "${ROOT}" /usr/bin/apt-get --allow-unauthenticated -y install $PACKAGES
 
 # Download and install riscv.h and riscv-opc.h - these are really needed only for
 # RISC-V development, but won't hurt having them.
